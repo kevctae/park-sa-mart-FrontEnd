bee12486718b60426260ff82c919cab8e14332d6
kevctae <kv.saetang@gmail.com>
2021-02-21 22:34:02 +0700

    Finish removing unused page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from "@angular/core";
-import { AuthRoutingModule } from "./auth-routing.module";
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { LogInComponent } from './log-in/log-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { GuestComponent } from './guest/guest.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import { RouterModule } from '@angular/router';
+
+
 
 @NgModule({
-    declarations: [
-        LandingPageComponent,
-        LogInComponent,
-        SignUpComponent,
-        GuestComponent,
-    ],
-    imports: [
-        AuthRoutingModule,
-    ]
+  declarations: [
+    AuthComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: AuthComponent,
+      }
+    ])
+  ]
 })
-export class AuthModule {}
+export class AuthModule { }
