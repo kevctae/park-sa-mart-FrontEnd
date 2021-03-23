ae0397d39f5a664768099135807bd1c6f2c634d7
kevctae <kv.saetang@gmail.com>
2021-03-23 19:09:03 +0700

    Create enum and template selection

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+enum Pages {
+  landingPage,
+  signIn,
+  signUp,
+  guest
+}
+
 @Component({
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+  page: Pages = Pages.landingPage;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.page == Pages.landingPage);
   }
 
+  public get Pages() {
+    return Pages; 
+ }
+
+  checkPage(page: number) {
+    return true;
+  }
+
+  goToLandingPage() {
+    this.page = Pages.landingPage;
+  }
+
+  goToSignIn() {
+    this.page = Pages.signIn;
+  }
+
+  goToSignUp() {
+    this.page = Pages.signUp;
+  }
+
+  goToGuest() {
+    this.page = Pages.guest;
+  }
 }
