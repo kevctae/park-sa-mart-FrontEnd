22b323be63fd93b950fb436005a67fcc90d3a88a
Kevin <kv.saetang@gmail.com>
2021-04-16 13:07:41 +0700

    Finish Edit account

--- src/app/pages/account/edit-account/edit-account.component.ts
+++ src/app/pages/account/edit-account/edit-account.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+import { AccountService } from 'src/app/core/services/account.service';
+import { Account } from '../../../core/models/account.model'
 
 @Component({
   selector: 'app-edit-account',
   templateUrl: './edit-account.component.html',
   styleUrls: ['./edit-account.component.scss']
 })
-export class EditAccountComponent implements OnInit {
+export class EditAccountComponent implements OnInit, OnDestroy {
+  account: Account | null = null;
+  account$: Subscription = new Subscription;
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.initializeAccount();
+  }
+  
+  initializeAccount() {
+    this.account$ = this.accountService.account.subscribe(account => {
+      this.account = account;
+    });
+  }
+
+  ngOnDestroy() {
+    this.account$.unsubscribe();
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value.npassword == '');
+    if (form.value.npassword == '') {
+      this.editProfile(
+        form.value.email,
+        form.value.password,
+        form.value.password,
+        form.value.fname,
+        form.value.lname,
+      )
+    } else {
+      this.editProfile(
+        form.value.email,
+        form.value.password,
+        form.value.npassword,
+        form.value.fname,
+        form.value.lname,
+      )
+    }
+   
+  }
+  
+  private editProfile(email: string, old_password: string, password: string, fname: string, lname: string) {
+    this.authService.editProfile(email, old_password, password, fname, lname)
+      .subscribe(
+      resData => {
+          this.router.navigate(['/account'], { skipLocationChange: true });
+      },
+      errorMessage => {
+          console.log(errorMessage);
+      }
+    );
   }
 
 }
