cb6033670becf47594812acbb91f5603c6a8d8d4
Kevin <kv.saetang@gmail.com>
2021-04-25 23:25:51 +0700

    Finish modal

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
+import { DOCUMENT } from '@angular/common';
 
 enum Pages {
   landingPage,
@@ -15,14 +17,26 @@ enum Pages {
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, AfterViewInit {
   page: Pages = Pages.landingPage;
   isLoading = false;
   error: string | null = null;
+  closeResult = '';
+  @ViewChild('content') el: ElementRef | undefined;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private modalService: NgbModal,
+  ) { }
 
   ngOnInit(): void { }
+
+  ngAfterViewInit() {
+    if (this.el != undefined) {
+      this.openModal(this.el);
+    }
+  }
 
   public get Pages() {
     return Pages; 
@@ -94,5 +108,27 @@ export class AuthComponent implements OnInit {
         this.isLoading = false;
       }
     );
+  }
+
+  openModal(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  goToUrl(): void {
+    document.location.href = 'https://stackoverflow.com';
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
